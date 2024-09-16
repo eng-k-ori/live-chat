c1489a9e922e6492bfb0de5e5027bf453bd82f47
eng-k-ori <172562164+eng-k-ori@users.noreply.github.com>
2024-09-16 15:41:19 +0900

    add:up-to-chapter9-2

--- live-chat-vuejs/src/main.js
+++ live-chat-vuejs/src/main.js
@@ -2,7 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/main.css'
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { faHeart } from "@fortawesome/free-solid-svg-icons"
 
+library.add(faHeart)
 // Vue.jsアプリが動くときに、このmain.jsが最初に読み込まれます。
 // Vueインスタンスが生成
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).component("font-awesome-icon", FontAwesomeIcon).mount('#app')
+
+
